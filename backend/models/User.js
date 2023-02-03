9dc69b177fc6b31d86d013bc3860f57c6e0c8c40
rishab negi <rishabnegi177@gmail.com>
2023-02-03 18:56:59 +0530

    user.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,6 +15,11 @@ const userSchema = new Schema({
         default:""
 
     }],
+    studentplaced:[{
+        type:Array,
+        default:""
+
+    }],
     lastName: {
         type: String,
         trim: true,
